93335666088f46f922e6f4a84ae5fc5daeb010c9
Milagros <milagrosbabuara@gmail.com>
2025-01-14 20:58:35 -0500

    clase 14 de enero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import {register} from 'swiper/element/bundle'; //importamos el registro del swiper 
 
+register(); //registramos el swiper
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
