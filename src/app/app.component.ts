90b6cc1dc78eba46c87ce773d11c68875033d2a6
Milagros <milagrosbabuara@gmail.com>
2025-01-15 21:03:16 -0500

    15Enero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {register} from 'swiper/element/bundle'; //importamos el registro del swiper 
-
+import { Storage } from '@ionic/storage-angular';
 register(); //registramos el swiper
 @Component({
   selector: 'app-root',
@@ -9,5 +9,8 @@ register(); //registramos el swiper
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private storage: Storage) {}
+  async ngOnInit() {
+    await this.storage.create();
+  }
 }
