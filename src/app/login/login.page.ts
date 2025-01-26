bbf9e67a800ace0de624cd918125c5a7dcba146d
Milagros <milagrosbabuara@gmail.com>
2025-01-26 00:26:41 -0500

    menu

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -51,7 +51,7 @@ export class LoginPage implements OnInit {
       console.log(res);
       this.errorMessage='';
       this.storage.set('isUserLoggedIn', true);
-      this.navCtrl.navigateForward('/home');
+      this.navCtrl.navigateForward('/menu/home');
 
     }).catch(err=>{
       console.log(err);
